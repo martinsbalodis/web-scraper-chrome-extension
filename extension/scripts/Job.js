809133e89682046a9a8acd30df39359630804f2f
Martins Balodis <martins256@gmail.com>
2014-07-13 01:11:14 +0300

    fix a special case where url merging failed

--- extension/scripts/Job.js
+++ extension/scripts/Job.js
@@ -16,31 +16,31 @@ Job.prototype = {
 
 	combineUrls: function (parentUrl, childUrl) {
 
-		var urlMatcher = new RegExp("(https?://)?([a-z0-9\\-\\.]+\\.[a-z]+)?(/.*)?(.*)?", "i");
+		var urlMatcher = new RegExp("(https?://)?([a-z0-9\\-\\.]+\\.[a-z]+)?(\\/[^\\?]*\\/|\\/)?([^\\?]*)?(\\?.*)?", "i");
 
 		var parentMatches = parentUrl.match(urlMatcher);
 		var childMatches = childUrl.match(urlMatcher);
 
 		// special case for urls like this: ?a=1  or like-this/
-		if (childMatches[1] === undefined && childMatches[2] === undefined && childMatches[3] === undefined) {
+		if (childMatches[1] === undefined && childMatches[2] === undefined && childMatches[3] === undefined && childMatches[4] === undefined) {
 
-			var url = parentMatches[1] + parentMatches[2] + parentMatches[3] + childMatches[4];
+			var url = parentMatches[1] + parentMatches[2] + parentMatches[3] + parentMatches[4] + childMatches[5];
 			return url;
 		}
 
-		for (var i = 1; i <= 2; i++) {
+		for (var i = 1; i <= 3; i++) {
 			if (childMatches[i] === undefined) {
 				childMatches[i] = parentMatches[i];
 			}
 		}
-		if (childMatches[3] === undefined) {
-			childMatches[3] = "/";
-		}
 		if (childMatches[4] === undefined) {
 			childMatches[4] = "";
 		}
+		if (childMatches[5] === undefined) {
+			childMatches[5] = "";
+		}
 
-		return childMatches[1] + childMatches[2] + childMatches[3] + childMatches[4];
+		return childMatches[1] + childMatches[2] + childMatches[3] + childMatches[4] + childMatches[5];
 	},
 
 	execute: function (browser, callback, scope) {
--- tests/spec/JobSpec.js
+++ tests/spec/JobSpec.js
@@ -23,4 +23,11 @@ describe("Job", function () {
 		expect(child.url).toBe("http://example.com/1/2/");
 
 	});
+
+	it("should be able to create correct url from parent job with slashes after question mark", function () {
+
+		var parent = new Job("http://www.sportstoto.com.my/results_past.asp?date=5/1/1992");
+		var child = new Job("popup_past_results.asp?drawNo=418/92", null, null, parent);
+		expect(child.url).toBe("http://www.sportstoto.com.my/popup_past_results.asp?drawNo=418/92");
+	});
 });
